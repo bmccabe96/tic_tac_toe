17a922e5dc7982e97aef3ffa6c0f9ef99c2629cd
bmccabe96 <brianmccabe96@gmail.com>
2022-01-05 09:23:25 -0500

    Fixed bug where game results in Tie from person winning on very last possible move

--- js/script.js
+++ js/script.js
@@ -120,15 +120,15 @@ const gameController = (() => {
     const checkForWinner = () => {
         console.log(gameBoard.getBoard());
         let board = gameBoard.getBoard();
-        if (board[0] === board[1] && board[0] === board[2] && board[0] !== "") {winnerFound();}
-        else if (board[0] === board[3] && board[0] === board[6] && board[0] !== "") {winnerFound();}
-        else if (board[0] === board[4] && board[0] === board[8] && board[0] !== "") {winnerFound();}
-        else if (board[1] === board[4] && board[1] === board[7] && board[1] !== "") {winnerFound();}
-        else if (board[2] === board[5] && board[2] === board[8] && board[2] !== "") {winnerFound();}
-        else if (board[2] === board[4] && board[2] === board[6] && board[2] !== "") {winnerFound();}
-        else if (board[3] === board[4] && board[3] === board[5] && board[3] !== "") {winnerFound();}
-        else if (board[6] === board[7] && board[6] === board[8] && board[6] !== "") {winnerFound();}
-        if (!board.includes("")){tie();}
+        if (board[0] === board[1] && board[0] === board[2] && board[0] !== "") {winnerFound(); return;}
+        else if (board[0] === board[3] && board[0] === board[6] && board[0] !== "") {winnerFound(); return;}
+        else if (board[0] === board[4] && board[0] === board[8] && board[0] !== "") {winnerFound(); return;}
+        else if (board[1] === board[4] && board[1] === board[7] && board[1] !== "") {winnerFound(); return;}
+        else if (board[2] === board[5] && board[2] === board[8] && board[2] !== "") {winnerFound(); return;}
+        else if (board[2] === board[4] && board[2] === board[6] && board[2] !== "") {winnerFound(); return;}
+        else if (board[3] === board[4] && board[3] === board[5] && board[3] !== "") {winnerFound(); return;}
+        else if (board[6] === board[7] && board[6] === board[8] && board[6] !== "") {winnerFound(); return;}
+        if (!board.includes("")){tie(); return;}
     }
 
     const tie = () => {
